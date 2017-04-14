efdca1a363da16e7948ffea313f509dd67066df9
betterwmj <happytuw@163.com>
2017-04-14 20:48:32 +0800

    PostStudentQuestion学生发布提问api

--- XUEXI/src/com/wmj/dao/OperatorOnline.java
+++ XUEXI/src/com/wmj/dao/OperatorOnline.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.wmj.bean.OnlineQuestion;
+import com.wmj.bean.Students;
 import com.wmj.util.JDBCUtil;
 
 public class OperatorOnline {
@@ -56,6 +57,42 @@ public class OperatorOnline {
 			JDBCUtil.close(conn, pmt);
 		}
 	return list;
- }
+   }
+	/*
+	 * 学生提问，若提交成功，返回true，否则返回false 
+	 */
+	public static boolean insertQuestion(OnlineQuestion question) throws Exception{
+		boolean result = false;
+		 //数据库连接的获取的操作，对用的是自己封装的一个util包中的类进行的操作
+		Connection conn = null;
+		try {
+			conn = JDBCUtil.getConnection();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			throw e1;
+		}
+		PreparedStatement pmt = null; 
+		try {
+			
+            String sql="insert into online_question (student_id,questiontitle,questioncontent,answer_id,createtime) values(?,?,?,?,?)";
+			pmt=JDBCUtil.getPreparedStatement(conn, sql); 
+			pmt.setInt(1, question.getStudentId());
+			pmt.setString(2, question.getQuestionTitle());
+			pmt.setString(3, question.getQuestionContent());
+			pmt.setInt(4, question.getAnswerId());
+			pmt.setTimestamp(5, question.getCreateTime());
+			if(pmt.executeUpdate()>0)
+			   result = true;
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			// 关闭连接
+			JDBCUtil.close(conn, pmt);
+		}
+		return result;
+	}
 
 }
