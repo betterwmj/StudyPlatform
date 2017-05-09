7c0208001694f15f7ddc6ec3f104e1a9a3a14fd9
chenzhuosteve <shadowan@foxmail.com>
2017-05-09 18:41:32 +0800

    1.学生查看自己历史提问

--- XUEXI/WebContent/html/component/studentQuestionHistory/studentQuestionHistory.js
+++ XUEXI/WebContent/html/component/studentQuestionHistory/studentQuestionHistory.js
@@ -7,6 +7,14 @@ export default function root(app){
   });
 }
 function controller($scope,$element,$state,$cookies,http){
-	
-
+	let vm = this;
+  vm.$onInit = async function(){
+    try {
+      vm.questionList = await http.get("GetStudentQuestion");
+    } catch (error) {
+      http.alert({
+        parent:$element,content:"获取历史数据失败"
+      });
+    }
+  };
 }
