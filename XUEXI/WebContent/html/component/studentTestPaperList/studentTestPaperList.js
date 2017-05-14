e678834657b4d174392a507ec4164a05310d73fc
chenzhuosteve <shadowan@foxmail.com>
2017-05-14 21:47:43 +0800

    1.添加图标

--- XUEXI/WebContent/html/component/studentTestPaperList/studentTestPaperList.js
+++ XUEXI/WebContent/html/component/studentTestPaperList/studentTestPaperList.js
@@ -78,15 +78,30 @@ function controller($scope,$element,$state,$cookies,http,$stateParams){
     }
   }
   vm.downloadPaper =async function(paperId){
-	  let result = await http.get("DownLoadPaper",{paperID:paperId});
-	  vm.paperLink = result;
-	  http.alert({
-			parent:$element,content:"试卷生成成功，文件路径如下：\n" +  vm.paperLink + "\n"
-			
-	  });
-	  vm.isDownload =true;  
+    try {
+      let result = await http.get("DownLoadPaper",{paperID:paperId});
+      vm.paperLink = result;
+      http.alert({
+        parent:$element,content:"试卷生成成功，文件路径如下：\n" +  vm.paperLink + "\n"
+      })
+      .then(function(){
+          let downLink = document.getElementById("download_paper");
+          let event = document.createEvent("MouseEvents"); 
+          event.initEvent("click", false, false); 
+          downLink.dispatchEvent(event);
+      });
+    } catch (error) {
+      http.alert({
+        parent:$element,content:"生成试卷异常"
+      });
+    }
+	  
   }
   $scope.$on("ready_back",function(){
     $state.go("student.test");
   });
+
+  vm.openMenu = function($mdMenu, ev){
+    $mdMenu.open(ev);
+  }
 }
