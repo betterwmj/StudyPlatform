d29fe9ed689e312054a7dfe0d0cbfa2c6c6e09fb
chenzhuosteve <shadowan@foxmail.com>
2017-05-21 01:03:32 +0800

    1.更新打包配置,gzip js code 2.配置tomcat filter response gzip file

--- 打包用配置/webpack.config.js
+++ 打包用配置/webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const webpack = require('webpack');
 let UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+let CompressionPlugin = require("compression-webpack-plugin");
 module.exports = {
     entry:{
         main:'./bootstrap.js',
@@ -37,6 +38,32 @@ module.exports = {
     plugins:[
 		
 		new UglifyJSPlugin({
+			exclude		  :	["bundle.js"],
+			comments      : false,
+			mangle		  : true,
+            sequences     : true,  // join consecutive statemets with the “comma operator”
+            properties    : true,  // optimize property access: a["foo"] → a.foo
+            dead_code     : true,  // discard unreachable code
+            drop_debugger : true,  // discard “debugger” statements
+            unsafe        : false, // some unsafe optimizations (see below)
+            conditionals  : true,  // optimize if-s and conditional expressions
+            comparisons   : true,  // optimize comparisons
+            evaluate      : true,  // evaluate constant expressions
+            booleans      : true,  // optimize boolean expressions
+            loops         : true,  // optimize loops
+            unused        : true,  // drop unused variables/functions
+            hoist_funs    : true,  // hoist function declarations
+            hoist_vars    : false, // hoist variable declarations
+            if_return     : true,  // optimize if-s followed by return/continue
+            join_vars     : true,  // join var declarations
+            cascade       : true,  // try to cascade `right` into `left` in sequences
+            side_effects  : true,  // drop side-effect-free statements
+            warnings      : true,  // warn about potentially dangerous optimizations/code
+            global_defs   : {}     // global definitions
+        }),
+        new UglifyJSPlugin({
+			exclude		  :	["vendor.bundle.js"],
+			comments      : false,
 			mangle		  : false,
             sequences     : true,  // join consecutive statemets with the “comma operator”
             properties    : true,  // optimize property access: a["foo"] → a.foo
@@ -58,10 +85,16 @@ module.exports = {
             warnings      : true,  // warn about potentially dangerous optimizations/code
             global_defs   : {}     // global definitions
         }),
-        
         new webpack.optimize.CommonsChunkPlugin({
             name: "vendor",
             filename: "vendor.bundle.js"
-        })   
+        }),
+		new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.(js)$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })		
     ]
 };
